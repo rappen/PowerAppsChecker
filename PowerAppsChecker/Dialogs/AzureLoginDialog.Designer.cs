1100a0a537792f403dc5d335269a959981518630
Jonas Rapp <jonas@jonasr.app>
2019-11-20 16:27:18 +0100

    Rename Azure ID terms

--- PowerAppsChecker/Dialogs/AzureLoginDialog.Designer.cs
+++ PowerAppsChecker/Dialogs/AzureLoginDialog.Designer.cs
@@ -95,9 +95,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(25, 56);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Client Id";
+            this.label2.Text = "Application ID";
             // 
             // txtClientId
             // 
@@ -122,9 +122,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(11, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Tenant Id";
+            this.label1.Text = "Directory ID";
             // 
             // panClientSecret
             // 
